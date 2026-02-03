f7b68a2a3c69a72d851ed9a290d708a960dc5378
Arijitghatak <arijitgrowww@gmail.com>
2026-02-03 19:37:39 +0530

    login page is yet to be done

--- src/employe/management/system/Login.java
+++ src/employe/management/system/Login.java
@@ -1,16 +1,24 @@
 package employe.management.system;
 
+import java.awt.Color;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener {
 
     JTextField tusername;
     JPasswordField tpassword;
+    JButton login,back;
 
     Login(){
 
+        setLayout(null);
+
         JLabel Username = new JLabel("Username");
         Username.setBounds(40,20,100,30);
+        Username.setForeground(Color.WHITE);
         add(Username);
 
         tusername = new JTextField();
@@ -19,6 +27,7 @@ public class Login extends JFrame {
 
         JLabel Password = new JLabel("Password");
         Password.setBounds(40,70,100,30);
+        Password.setForeground(Color.WHITE);
         add(Password);
 
         tpassword = new JPasswordField();
@@ -26,10 +35,37 @@ public class Login extends JFrame {
         add(tpassword);
 
 
+        login  = new JButton("Login");
+        login.setBounds(150,140,150,30);
+        add(login);
+
+        back  = new JButton("Back");
+        back.setBounds(150,180,150,30);
+        add(back);
+
+        ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.png"));
+        Image i22 = i11.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
+        ImageIcon i33 = new ImageIcon(i22);
+        JLabel image1 = new JLabel(i33);
+        image1.setBounds(0,0,730,230);
+        add(image1);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(600,300,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,0,600,300);
+        add(image);
+
         setSize(600,300);
         setLocation(450,200);
         setVisible(true);
-        setLayout(null);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 
